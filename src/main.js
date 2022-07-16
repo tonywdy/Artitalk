0556654851c98e90e1bc5ffa4dd3da2bc389bb6a
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-16 10:13:50 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -895,7 +895,7 @@ atEvery.prototype.beginUpload = function (file) {
             fadeOut('lazy');
         }
     });
-    xhr.open("POST", "https://7bu.top/api/upload");
+    xhr.open("POST", "https://www.helloimg.com/api/upload");
     let imgToken = AV.User.current().attributes.imgToken;
     if (imgToken !== undefined) {
         xhr.setRequestHeader("Authorization", "Basic VGVzdDoxMjM0NTY=");
